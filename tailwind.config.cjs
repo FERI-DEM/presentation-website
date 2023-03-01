08d98f7e814dab00660432db215da46328219bed
Matevz <semprimoznik.matevz@gmail.com>
2023-03-01 17:21:14 +0100

    feat: add header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				blue: '#0096D7',
+			},
+			maxWidth: {
+				'container': '1350px'
+			}
+		},
+		container: {
+			center: true,
+		},
 	},
 	plugins: [],
 }
